c7fe855b910989558a8500c7b9ff7de23bd77b0d
ToasteeAus <stupidmarket@gmail.com>
2024-08-06 13:05:28 +1000

    Created BRWiFiModule for connection basics

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
 #include <Arduino.h>
-#include <brwifi.h>
+#include <WiFi.h>
+#include "BRWiFiModule.h"
 
 void setup() {
   Serial.begin(115200);
-  delay(1000); // Only required to ensure the Serial Monitor is initialised before we start pumping it with info
-  xTaskCreatePinnedToCore(init_wifi, "WiFiSetup", 10240, NULL, 3, NULL, 0);
-  delay(10);
+
+  BRWiFiSetup();
 }
 
 void loop() {
   delay(1000);
-  Serial.println(heartbeat());
+  BRWiFiHeartbeat();
 }
-
