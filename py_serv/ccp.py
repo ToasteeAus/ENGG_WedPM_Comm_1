b92cc7080217af870fbe7d1e57a8b61b20ac8b04
ToasteeAus <stupidmarket@gmail.com>
2024-08-25 16:15:34 +1000

    Implemented call and response for CCP -> ESP32 using JSON

--- py_serv/ccp.py
+++ py_serv/ccp.py
@@ -1,39 +1,138 @@
 import socket
+import json
+import time
 
-# Define the host and port for the server
+# CCP Scope Variables
+
 HOST = '0.0.0.0'  # Listen on all available interfaces
 PORT = 12345       # Port to listen on
 
-# Create a TCP socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+heartbeat_count = 0
 
-# Bind the socket to the specified host and port
-server_socket.bind((HOST, PORT))
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create global-ish scoped socket to reference
+client_socket = None
 
-# Start listening for incoming connections (max 1 connection in the queue)
-server_socket.listen(1)
+br_state = "OFF"
+###########################################################
 
-print(f"Server listening on {HOST}:{PORT}")
+def heartbeat():
+    global client_socket, heartbeat_count
+    if client_socket is not None:
+        data = {
+            "heartbeat_count": heartbeat_count,
+            "CMD":"SETUP"
+        }
+        
+        json_data = json.dumps(data)
+        
+        client_socket.sendall(json_data.encode('utf-8'))
+        print(f"Sent to client: {json_data}")
+    
+        heartbeat_count += 1
 
-# Accept a connection
-client_socket, client_address = server_socket.accept()
-print(f"Connection from {client_address}")
-
-try:
-    while True:
-        # Receive data from the client
+def echo():
+    global client_socket
+    if client_socket is not None:
         data = client_socket.recv(1024).decode('utf-8')
-        if not data:
-            break
-        print(f"Received from client: {data}")
+        if data:
+            print(f"Received from client: {data}")
 
-        # Send a response back to the client
-        response = f"Echo: {data}"
-        client_socket.sendall(response.encode('utf-8'))
+            # Send a response back to the client
+            response = f"Echo: {data}"
+            client_socket.sendall(response.encode('utf-8'))
+
+def setup_esp_socket():
+    # Bind the socket to the specified host and port
+    global client_socket, server_socket
+    server_socket.bind((HOST, PORT))
+
+    server_ip = socket.gethostbyname(socket.gethostname())
+    print(f"Server is hosting on IP address: {server_ip}:{PORT}")
+
+    # Start listening for incoming connections (max 1 connection in the queue)
+    server_socket.listen(1)
+
+    print(f"Server listening for BR28 on {HOST}:{PORT}")
+    # Accept a connection
+    client_socket, client_address = server_socket.accept()
+    print(f"Connection from {client_address}")
+    
+def setup_esp():
+    global client_socket, br_state
+    if client_socket is not None:
+        sent_data = {
+            "CMD":"SETUP"
+        }
+        
+        sent_json_data = json.dumps(sent_data)
+        
+        client_socket.sendall(sent_json_data.encode('utf-8'))
+        print(f"Sent to client: {sent_json_data}")
+        
+        # Receive back response from ESP32
+        data = client_socket.recv(1024).decode('utf-8')
+        
+        if data:
+            print(f"Received from client: {data}")
+
+            # Attempt to parse the JSON data
+            try:
+                json_data = json.loads(data)
+                
+                print("Parsed JSON data:", json.dumps(json_data, indent=4))
+                print(f"Returned Command: {json_data["CMD"]}")
+            except json.JSONDecodeError:
+                print("Failed to parse JSON")
+        # Insert ACK of response
+        # Confirm ACK back from ESP32
+        # Change State
+        br_state = "ON"
+    
+def status_esp():
+    global client_socket
+    if client_socket is not None:
+        sent_data = {
+            "CMD":"STATUS"
+        }
+        
+        sent_json_data = json.dumps(sent_data)
+        
+        client_socket.sendall(sent_json_data.encode('utf-8'))
+        print(f"Sent to client: {sent_json_data}")
+        
+        # Receive back response from ESP32
+        data = client_socket.recv(1024).decode('utf-8')
+        
+        if data:
+            print(f"Received from client: {data}")
+
+            # Attempt to parse the JSON data
+            try:
+                json_data = json.loads(data)
+                
+                print("Parsed JSON data:", json.dumps(json_data, indent=4))
+                print(f"Returned Status: {json_data["STATUS"]}")
+            except json.JSONDecodeError:
+                print("Failed to parse JSON")
+        # Insert ACK of response
+        # Confirm ACK back from ESP32
+        
+try:
+    setup_esp_socket()
+    
+    while True:
+        match br_state:
+            case "OFF":
+                setup_esp()
+            case "ON":
+                status_esp()
+            case _:  
+                heartbeat()
+        time.sleep(1)
 
 finally:
     # Close the connection with the client
-    client_socket.close()
-
-# Close the server socket
-server_socket.close()
+    if client_socket is not None:
+        client_socket.close()
+    # Close the server socket
+    server_socket.close()
---
+++ src/main.ino
@@ -0,0 +1,89 @@
+#include <WiFi.h>
+#include <ArduinoJson.h>
+
+// WiFi credentials
+const char* ssid     = "AndroidAFB94"; // Replace with LAN name and pass
+const char* password = "Test1234";
+
+// Server address and port
+const char* serverIP = "192.168.125.177";  // Replace with the IP address of your local python server
+const uint16_t serverPort = 12345;
+
+void setup() {
+  // Initialize Serial
+  Serial.begin(115200);
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void loop() {
+  // Connect to the TCP server
+  static WiFiClient client;
+  if (client.connect(serverIP, serverPort)) {
+    Serial.println("Connected to server");
+
+    // Buffer for receiving JSON data
+    char buffer[512];
+    
+    // Read data from the server
+    while (client.connected()) {
+      if (client.available()) {
+        // Read the incoming JSON data
+        int length = client.readBytesUntil('\n', buffer, sizeof(buffer));
+        buffer[length] = '\0'; // Null-terminate the string
+
+        // Parse the JSON data
+        StaticJsonDocument<512> doc;
+        DeserializationError error = deserializeJson(doc, buffer);
+
+        if (error) {
+          Serial.print("Failed to parse JSON: ");
+          Serial.println(error.c_str());
+        } else {
+          // Print the parsed JSON data
+          Serial.println("Received JSON:");
+          serializeJsonPretty(doc, Serial);
+          Serial.println();
+
+          if(doc["CMD"] == "SETUP"){
+            // Create JSON data to send - Refer to ArduinoJson Assistant: https://arduinojson.org/v6/assistant/
+            StaticJsonDocument<200> replydoc;
+            replydoc["CMD"] = "SETUP_OK";
+
+            // Serialize JSON data to a string
+            String json_data;
+            serializeJson(replydoc, json_data);
+
+            // Send the JSON data to the server
+            client.println(json_data);
+            Serial.println("Sent JSON to server:");
+            Serial.println(json_data);
+          } else if (doc["CMD"] == "STATUS"){
+            // Create JSON data to send - Refer to ArduinoJson Assistant: https://arduinojson.org/v6/assistant/
+            StaticJsonDocument<200> replydoc;
+            replydoc["STATUS"] = "NORMINAL";
+
+            // Serialize JSON data to a string
+            String json_data;
+            serializeJson(replydoc, json_data);
+
+            // Send the JSON data to the server
+            client.println(json_data);
+            Serial.println("Sent JSON to server:");
+            Serial.println(json_data);
+          }
+        }
+      }
+    }
+    client.stop();
+    Serial.println("Disconnected from server");
+  } else {
+    Serial.println("Connection to server failed");
+  }
+}
